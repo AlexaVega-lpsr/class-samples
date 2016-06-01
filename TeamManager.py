2f8d102a38de35b7a9634777c1d393ffbff37b28
Alexa Vega <rc.vega.alexa@leadps.org>
2016-06-01 14:05:03 -0700

    Update TeamManager.py

--- TeamManager.py
+++ TeamManager.py
@@ -8,22 +8,21 @@ class Player(object):
 		print("Age: " + self.age)
 		print("Goals: " + self.goals)
 myPlayers = []
-nanalan = raw_input()
+nanalan = []
 while nanalan != '3':
 	print("Choose an option:")
 	print("(1) Add player")
 	print("(2) Print players")
 	print("(3) End")
-	nanalan = raw_input()
+	nanalan = input()
 	if nanalan == '1':
 		print("Enter name:")
-		user_entered_name = raw_input()
+		user_entered_name = input()
 		print("Enter age:")
-		user_entered_age = raw_input()
+		user_entered_age = input()
 		print("Enter goals:")
-		user_entered_goals = raw_input()
+		user_entered_goals = input()
 		myPlayers.append(Player(user_entered_name, user_entered_age, user_entered_goals))
 	if nanalan == '2':
 		for Player in myPlayers:
 			Player.printStats()
-
