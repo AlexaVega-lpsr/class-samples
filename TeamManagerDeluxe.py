9ce4acfdc564e8b78deccd13d8e7c84f9d55e096
Alexa Vega <rc.vega.alexa@leadps.org>
2016-06-10 17:40:28 -0700

    Update TeamManagerDeluxe.py
    I have revised Team Manager Deluxe everything works except loading an existing save file.

--- TeamManagerDeluxe.py
+++ TeamManagerDeluxe.py
@@ -1,41 +1,44 @@
+import time
+
 class Player(object):
-    def __init__(self, name, age, goals): 
+	def __init__(self, name, age, goals, jn, pos): 
  
-        self.name = name
-        self.age = age
-        self.goals = goals
-        self.jn = jn
-        self.pos = pos
-    def printStats(self):
-        print("Name: " + self.name)
-        print("Age: " +  self.age)
-        print("Goals: " + self.goals)
-        print("Jersey Number: " + self.jn)  
-        print("Position:" + self.pos)
-        print(" ")
-  def saveTeam(playerList, filename):
-        # open the file
-        file = open((filename), "a")
-        # write it to the file
-        for o in nlist:
-                file.write(o.name + " "  + str(o.age) + " " + str(o.goals) + " " + str(o.jn) + " " + str(o.pos) + " ")
-        # close the file
-        file.close()
+        	self.name = name
+        	self.age = age
+        	self.goals = goals
+        	self.jn = jn
+        	self.pos = pos
+	def printStats(self):	
+	       print("Name: " + self.name)
+ 	       print("Age: " +  self.age)
+ 	       print("Goals: " + self.goals)
+ 	       print("Jersey Number: " + self.jn)  
+ 	       print("Position:" + self.pos)
+ 	       print(" ")
+
+def saveTeam(playerList, filename):
+ 	# open the file
+	file = open((filename), "a")
+ 	# write it to the file
+ 	for o in nlist:
+ 		file.write(o.name + " "  + str(o.age) + " " + str(o.goals) + " " + str(o.jn) + " " + str(o.pos) + " ")
+	# close the file
+	file.close()	 
  
  
 #takes a file name for a file of players
 #returns a list of Players
-        def LoadTeam(filename): 
-                list = []
-                file = open((filename), "r")
-                wrds = filename.readline()
-                while wrds != " ":
-                        wrds.split()
-                        stry = wrds.split()
-                        list.append(Player(stry[0], stry[1], stry[2], stry[3], stry[4])
-                        wrds = file.readline()
-                file.close
-                return list
+def LoadTeam(filename, file): 
+	list = []
+        file = open(filename, "w", )
+	wrds = file.readline()
+	while file != " ":
+		file.split()
+		stry = file.split()
+		list.append(Player(stry[0], stry[1], stry[2], stry[3], stry[4]))
+		file = filename.readline()
+	file.close()
+	return list
  
  
 print("Hello! Welcome to Team Manager Deluxe!" '\n' "Enter the 3 of your choice and press Enter.")
@@ -44,9 +47,10 @@ answer_choice = int(raw_input())
  
  
 if answer_choice == 2:
-        print("What's the filename for your existing team? Enter the whoe name, including its .tmd extension.")
+        print("What's the filename for your existing team? Enter the whole name, including its .tmd extension.")
         filename = raw_input()
-        my_players = loadTeam(filename)
+	file = open(filename, "w")
+	my_players = LoadTeam(filename, file)
         print("Success!... What now...?")
 else:
         my_players = []
@@ -65,8 +69,8 @@ print("(4) Exit and Delete Players")
 time.sleep(.2)
  
  
- 
-while a != "0":
+a = raw_input() 
+while a != "4":
  
         if a == "1":
  
@@ -81,18 +85,18 @@ while a != "0":
  
                 print("Enter Age: ")
  
-                age = input()
+                age = raw_input()
  
  
                 print("Enter Goals: ")
  
-                goals = input()
+                goals = raw_input()
  
  
  
                 print("Enter Jersey Number: ")
  
-                jn = input()
+                jn = raw_input()
  
  
  
@@ -104,7 +108,7 @@ while a != "0":
 # saves name, age, and goals to their profile and adds to list
  
                 nlist.append(Player(name, age, goals, jn, pos))
-                                time.sleep(.5)
+                time.sleep(.5)
  
                 print(" ")
  
@@ -153,7 +157,7 @@ while a != "0":
                 time.sleep(.5)
  
                 for p in nlist:
-                         p.printStats()
+                        p =  p.printStats()
  
                         time.sleep(.5)
  
@@ -186,7 +190,7 @@ while a != "0":
  
                 time.sleep(.1)
  
-                print("(0) Exit and delete players")
+                print("(4) Exit and delete players")
  
                 time.sleep(.1)
  
@@ -203,20 +207,22 @@ while a != "0":
  
                 filename = raw_input()  
  
-                saveTeam(list, filename)
+                x = saveTeam(list, filename)
  
-                nlist.append(list)
- 
+		nlist.append(x)
+
                 time.sleep(.3)
  
-                print("Saved it!  Press (0) to exit. Restart the program to view your team.")
+                print("Saved it!  Press (4) to exit. Restart the program to view your team.")
  
                 a  = raw_input()
  
-# if they enter 0
+# if they enter 4
  
-if a  == "0":
+	if a  == "4":
  
-        time.sleep(.5)
+        	time.sleep(.5)
  
-        print("Good bai")
+	        print("Good bai")
+
+		a = "4"
